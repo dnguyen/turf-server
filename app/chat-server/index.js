ca59bf1d9af12affcbf945fe61bfb502412b807f
Dylan Nguyen <d_nguyen@live.com>
2014-02-14 14:50:13 -0500

    Add disconnect event for chat server

--- app/chat-server/index.js
+++ app/chat-server/index.js
@@ -16,11 +16,22 @@ module.exports = function(server) {
 		socket.on('connect_', function(data) {
 			console.log('recv connect_');
 			Users.get(data.uid).then(function(user) {
+				// Store user id in the socket and redis.
+				socket.set('userid', user.id, function() {});
 				redis.client.hset('online_users', user.id, JSON.stringify(user), function(err, rsp) {
 					console.log('Stored ' + user.id + ' in online_users hash');
 				});
 			}).error(function(err) {
 				console.log(err);
+			});
+		});
+
+		socket.on('disconnect', function(data) {
+			socket.get('userid', function(err, userid) {
+				if (err) throw err;
+				redis.client.hdel('online_users', userid, function() {
+					console.log('Removed ' + userid + ' from online_users');
+				});
 			});
 		});
 
